0772723093024125dae410478e019790809dddbd
Vlad <vlad.rasya.02@gmail.com>
2023-01-28 19:31:49 +0500

    Create empty window

---
+++ src/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+#include "glad/gl.h"
+#include "GLFW/glfw3.h"
+
+void ErrorCallback(int error_code, const char *message)
+{
+    std::cout << "ERROR: " << message << "\nERROR CODE: " << error_code << std::endl;
+}
+
+void KeyCallback(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+int main()
+{
+    glfwSetErrorCallback(ErrorCallback);
+
+    if (!glfwInit())
+    {
+        std::cout << "LAUNCH ERROR: Glfw initialization failed" << std::endl;
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    unsigned int width = 1280;
+    unsigned int height = 720;
+    GLFWwindow *window = glfwCreateWindow(width, height, "coursework-2", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "LAUNCH ERROR: Window creation failed" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+    int gl_version = gladLoadGL(glfwGetProcAddress);
+    if (gl_version == 0)
+    {
+        std::cout << "LAUNCH ERROR: Glad initialization failed" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwSetKeyCallback(window, KeyCallback);
+
+    glViewport(0, 0, width, height);
+    glEnable(GL_DEPTH_TEST);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+}
